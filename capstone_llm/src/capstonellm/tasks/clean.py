855e62a82f84eff549fca722c9da038dc2338aaf
Romain Mormont <romainmormont@hotmail.com>
2024-09-12 14:13:17 +0000

    feat: implement cleaning

--- capstone_llm/src/capstonellm/tasks/clean.py
+++ capstone_llm/src/capstonellm/tasks/clean.py
@@ -1,13 +1,56 @@
+import os
 import argparse
 import logging
+import boto3
 from pyspark.sql import SparkSession
-
+import pyspark.sql.functions as psf
 from capstonellm.common.spark import ClosableSparkSession
 
 logger = logging.getLogger(__name__)
 
+
 def clean(spark: SparkSession, environment: str, tag: str):
-    pass
+    bucket = "dataminded-academy-capstone-llm-data-us"
+    input_folder = os.path.join(bucket, "input")
+
+    tags = ["airflow", "apache-spark", "dbt", "docker", "pyspark", "python-polars", "sql"]
+    
+    for tag in tags:
+        questions_raw = spark.read.json(os.path.join(f"s3a://{input_folder}", tag, "questions.json"))
+        questions_expl = questions_raw.withColumn("item", psf.explode(psf.col("items")))
+        questions = (
+            questions_expl.select(
+                psf.col("item.accepted_answer_id"),
+                psf.col("item.body").alias("question_body"),
+                psf.col("item.question_id"),
+                psf.col("item.title").alias("question_title"),
+            )
+        )
+
+        answers_raw = spark.read.json(os.path.join(f"s3a://{input_folder}", tag, "answers.json"))
+        answers_expl = answers_raw.withColumn("item", psf.explode(psf.col("items")))
+        answers = answers_expl.select(
+            psf.col("item.answer_id").alias("accepted_answer_id"),
+            psf.col("item.body").alias("answer_body")
+        )
+
+        content = questions.join(
+            answers,
+            on="accepted_answer_id"
+        ).select(
+            "question_id",
+            psf.concat_ws(os.linesep, "question_title", "question_body", "answer_body").alias("content")
+        )
+         
+        s3_client = boto3.client('s3')
+        for row in content.collect():
+            s3_client.put_object(Bucket=bucket, Key=f"cleaned/{tag}/{row['question_id']}.txt", Body=row['content'])
+
+
+
+
+        
+    
 
 def main():
     parser = argparse.ArgumentParser(description="capstone_llm")
